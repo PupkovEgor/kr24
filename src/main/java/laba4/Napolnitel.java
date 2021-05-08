0f57fad76bd34712df12fd1c9773916f86b252f6
Pupkov <User@User-PC>
2021-05-08 11:06:05 +0500

    Пупков добавил класс Napolnitel.java

---
+++ src/main/java/laba4/Napolnitel.java
@@ -0,0 +1,12 @@
+package laba4;
+
+public class Napolnitel extends Materials {
+	@Override
+	public void setName(String name) {
+	       this.name = name;
+	   }
+	@Override
+	public void setPrice(String price) {
+		this.price = price;
+	}
+}
